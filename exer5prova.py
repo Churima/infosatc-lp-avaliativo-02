ad6158c7869aa926a3f2673e73dfdda18a8c0c87
Churima <89033866+Churima@users.noreply.github.com>
2021-09-06 16:20:38 -0300

    Create exer5prova.py

---
+++ exer5prova.py
@@ -0,0 +1,18 @@
+listaFilmes = ['Vingadores','Hulk','Batman','Liga da Justiça','Super-Man']
+listaJogos = ['League of Legends','Valorant','Mortal Kombat','Sonic','Mario']
+listaLivros = ['Senhos dos Aneis','Assassins creed','A culpa é das estrelas','The Witcher']
+listaEsportes = ['Futebol','Handebol','Basquete','Volei','Corrida']
+
+listaRei = [listaFilmes, listaJogos, listaLivros, listaEsportes]
+
+print(listaLivros[3])
+
+listaEsportes.remove("Futebol")
+
+print(listaEsportes)
+
+listaDiciplina = ['Geografia','Matematica','Gramatica','Historia']
+
+listaRei.append(listaDiciplina)
+
+print(listaRei)
