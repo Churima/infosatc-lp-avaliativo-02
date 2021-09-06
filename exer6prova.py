00bf1e91c78e124c72782acc99cc0be6d77eabda
Churima <89033866+Churima@users.noreply.github.com>
2021-09-06 16:28:26 -0300

    Create exer6prova.py

---
+++ exer6prova.py
@@ -0,0 +1,8 @@
+listaPC = ['Gabinete Cooler Master','GeForce RTX 2080Ti','Processador I5','Placa Mãe asus TUF','Memória RAM 8GB','Fonte 500w','HD 1T']
+
+listaPC.remove("Gabinete Cooler Master")
+listaPC.remove("GeForce RTX 2080Ti")
+
+listaPC.append("Gabinete ATX")
+listaPC.append("GeForce GT 710")
+print(listaPC)
